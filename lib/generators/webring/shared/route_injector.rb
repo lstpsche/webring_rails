1f45aafcabb50fe358bf88c0323d75e9fc6e7cd8
Nikita Silivonchik <lstpsche@gmail.com>
2025-06-01 11:11:27 +0200

    Remove out-of-the-box admin panel

--- lib/generators/webring/shared/route_injector.rb
+++ lib/generators/webring/shared/route_injector.rb
@@ -17,6 +17,20 @@ module Webring
             route "#{cleared_route_content}\n"
           end
         end
+
+        def inject_routes_into_main_app(route_content)
+          cleared_route_content = route_content.gsub(/^/, '  ')
+          routes_file = 'config/routes.rb'
+          mount_point = "mount Webring::Engine => '/webring', as: 'webring'\n"
+
+          if File.read(routes_file).include?(mount_point)
+            inject_into_file routes_file, after: mount_point do
+              "\n#{cleared_route_content}"
+            end
+          else
+            route "#{cleared_route_content}\n"
+          end
+        end
       end
     end
   end
