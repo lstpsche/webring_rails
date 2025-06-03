a68a15d1275881becc0869629230ab5658ea1f16
Nikita Silivonchik <lstpsche@gmail.com>
2025-06-03 23:01:24 +0200

    docs: Improve generators documentation

--- lib/generators/webring/install/install_generator.rb
+++ lib/generators/webring/install/install_generator.rb
@@ -1,14 +1,28 @@
 module Webring
   module Generators
+    # @description The InstallGenerator is the first step to set up the Webring engine
+    # It mounts the Webring engine to your Rails application's routes
+    #
+    # @usage Run: rails generate webring:install
+    #
+    # @example After installation, the engine will be mounted at /webring
+    #   # In your routes.rb
+    #   mount Webring::Engine => '/webring', as: 'webring'
+    #
+    # @note You can change the mount path in your routes.rb file after installation
     class InstallGenerator < Rails::Generators::Base
       source_root File.expand_path('templates', __dir__)
 
-      desc 'Creates Webring routes to your application.'
+      desc 'Creates Webring routes and mounts the engine in your application.'
 
+      # Adds the engine mount point to the application's routes.rb file
+      # @return [void]
       def add_webring_routes
         route "mount Webring::Engine => '/webring', as: 'webring'\n\n"
       end
 
+      # Displays the README with next steps after installation
+      # @return [void]
       def show_readme
         readme 'AFTER_INSTALL' if behavior == :invoke
       end
--- lib/generators/webring/member/member_generator.rb
+++ lib/generators/webring/member/member_generator.rb
@@ -4,28 +4,49 @@ require_relative '../shared/route_injector'
 
 module Webring
   module Generators
+    # @description The MemberGenerator creates the Member model, which is the core of the webring
+    # It creates both the model file and a migration to create the database table
+    #
+    # @usage Run: rails generate webring:member
+    #
+    # @example Generated Member model has the following attributes:
+    #   # uid  - A unique identifier for the member (automatically generated)
+    #   # name - The name of the member site (defaults to URL if not provided)
+    #   # url  - The URL of the member site (required)
+    #
+    # @note After running this generator, you should run the migration with:
+    #       rails db:migrate
     class MemberGenerator < Rails::Generators::Base
       include Rails::Generators::Migration
       include Shared::RouteInjector
 
       source_root File.expand_path('templates', __dir__)
 
-      desc 'Creates a Webring::Member model and necessary migration'
+      desc 'Creates a Webring::Member model and necessary migration for storing webring members'
 
+      # Creates a migration file to create the webring_members table
+      # @return [void]
       def create_migration_file
         migration_template 'migration.rb', 'db/migrate/create_webring_members.rb'
       end
 
+      # Creates the Member model file based on the template
+      # @return [void]
       def create_model_file
         template 'model.rb', 'app/models/webring/member.rb'
       end
 
-      # required by Rails::Generators::Migration
+      # Generates the next migration number for the migration file
+      # This is required by Rails::Generators::Migration
+      # @param dirname [String] The directory where migrations are stored
+      # @return [Integer] The next migration number
       def self.next_migration_number(dirname)
         next_migration_number = current_migration_number(dirname) + 1
         ActiveRecord::Migration.next_migration_number(next_migration_number)
       end
 
+      # Displays the README with next steps after installation
+      # @return [void]
       def show_readme
         readme 'AFTER_INSTALL' if behavior == :invoke
       end
