f9014a5db5a0ad0f59927b443fb7530344ceea37
Nikita Silivonchik <lstpsche@gmail.com>
2025-06-04 12:25:02 +0200

    Fix entry file name

---
+++ lib/webring-rails.rb
@@ -0,0 +1,7 @@
+require 'webring/version'
+require 'webring/engine'
+# require 'webring/navigation'
+
+module Webring
+  # autoload :Navigation, 'webring/navigation'
+end
--- lib/webring_rails.rb
+++
@@ -1,5 +0,0 @@
-require 'webring/version'
-require 'webring/engine'
-
-module Webring
-end
